fba006d7acb42730014bbea67b834410ac347f1b
Naz <nazeefsuleman@gmail.com>
2022-03-04 19:52:56 -0500

    Added the API and some designs, only thing left to do is complete the design

---
+++ main.js
@@ -0,0 +1,13 @@
+let adviceNumber = document.getElementById("advice-number");
+let advice = document.getElementById("advice");
+
+
+const apiData = fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => {
+
+        let receivedAdviceNb = data.slip.id;
+        let receivedAdvice = data.slip.advice;
+        adviceNumber.append(`ADVICE #${receivedAdviceNb}`);
+        advice.append(`${receivedAdvice}`);
+    })
